3ae7140300dad84e71d54abaced0bef53a9986b2
pedrohemmel <phenrique_h2016@hotmail.com>
2023-10-30 17:19:33 -0300

    Project created and code structure implemented

---
+++ MiniChallenge4/MiniChallenge4/Code/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniChallenge4
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 30/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniChallenge4/MiniChallenge4/SupportingFiles/MiniChallenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge4App.swift
+//  MiniChallenge4
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
