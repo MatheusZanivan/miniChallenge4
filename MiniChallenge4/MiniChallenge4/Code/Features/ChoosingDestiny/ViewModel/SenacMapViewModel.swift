7c81cfa575b3b5edc9ec9f80e9712b98b7186136
pedrohemmel <phenrique_h2016@hotmail.com>
2023-10-31 20:04:57 -0300

    Decoding data from senac json map

---
+++ MiniChallenge4/MiniChallenge4/Code/Features/ChoosingDestiny/ViewModel/SenacMapViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  SenacMapViewModel.swift
+//  MiniChallenge4
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 31/10/23.
+//
+
+import Foundation
+
+class SenacMapViewModel: ObservableObject {
+    @Published var senacMap = [SenacPlaceModel]()
+    
+    init() {
+        loadSenacMap()
+    }
+    
+    func loadSenacMap() {
+        if let senacMapFile = Bundle.main.path(forResource: "senac_map", ofType: "json") {
+            do {
+                guard let contentsOfSenacMapFile = try? String(contentsOfFile: senacMapFile).data(using: .utf8) else {
+                    print("Could not convert senac map data to string.")
+                    return
+                }
+                senacMap = try JSONDecoder().decode([SenacPlaceModel].self, from: contentsOfSenacMapFile)
+                
+            } catch {
+                print("Could not decode data in senac map file.")
+            }
+        } else {
+            print("Could not find senac map file.")
+        }
+    }
+}
