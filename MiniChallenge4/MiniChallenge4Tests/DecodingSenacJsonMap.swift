0702b4e4c28fd1d15ed81b78d02d69b5d6126701
pedrohemmel <phenrique_h2016@hotmail.com>
2023-10-31 20:35:21 -0300

    Testing decoded data from senac json map

---
+++ MiniChallenge4/MiniChallenge4Tests/DecodingSenacJsonMap.swift
@@ -0,0 +1,24 @@
+//
+//  DecodingSenacJsonMap.swift
+//  MiniChallenge4Tests
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 31/10/23.
+//
+
+import XCTest
+import Combine
+@testable import MiniChallenge4
+
+
+final class DecodingSenacJsonMap: XCTestCase {
+    func testSenacMapDataLoader() throws {
+        let senacMapViewModel = SenacMapViewModel()
+        var cancellables = Set<AnyCancellable>()
+        senacMapViewModel
+            .$senacMap
+            .sink(receiveValue: { newSenacMap in
+                XCTAssertNotNil(newSenacMap)
+            })
+            .store(in: &cancellables)
+    }
+}
