e50be25b08be924ecbf6206738c49dd2bfdea8f7
Matheus Zanivan <zanivanandrade@gmail.com>
2023-11-21 15:17:48 -0300

    feat:reconhecedor completo

--- MiniChallenge4/MiniChallenge4/Code/Features/ArtworkRecognizer/CameraViewControler.swift
+++ MiniChallenge4/MiniChallenge4/Code/Features/ArtworkRecognizer/CameraViewControler.swift
@@ -13,84 +13,79 @@ import Vision
 
 //temos que fazer com que o nosso AvFoundation tenha acesso à câmera, e precisamos ter acesso a cada frame dessa câmera
 //o nosso controler tem duas funcoes aqui, checar se a gente tem permissão para usar a câmera e depois seta uma capturesession
-class CameraViewControler : UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate{
-	private var permissionGranted = false //flag da permissão
-	private var captureSession = AVCaptureSession()
-	private var sessionQueue = DispatchQueue(label: "sessionQueue")
-	private var previewLayer = AVCaptureVideoPreviewLayer()
-	var screenRect : CGRect! = nil// para dimensao da view
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 	
-	//detection
-	private var videoOutput = AVCaptureVideoDataOutput()
-	var requests = [VNRequest]()
-	
-	
+	let identifierLabel: UILabel = {
+		let label = UILabel()
+		label.backgroundColor = .white
+		label.textAlignment = .center
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+
 	override func viewDidLoad() {
-		checkPermission()
+		super.viewDidLoad()
 		
-		sessionQueue.async { [unowned self] in
-			guard permissionGranted else { return }
-			self.setupCaptureSession()
-			self.captureSession.startRunning()
+		let captureSession = AVCaptureSession()
+		captureSession.sessionPreset = .photo
+		
+		guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+		guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+		captureSession.addInput(input)
+		
+		captureSession.startRunning()
+		
+		let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+		view.layer.addSublayer(previewLayer)
+		previewLayer.frame = view.frame
+		
+		let dataOutput = AVCaptureVideoDataOutput()
+		dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+		captureSession.addOutput(dataOutput)
+		
+		setupIdentifierConfidenceLabel()
+	}
+	
+	fileprivate func setupIdentifierConfidenceLabel() {
+		view.addSubview(identifierLabel)
+		identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+		identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+		identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+		identifierLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+	}
+	
+	func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+		
+		guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+		
+		guard let model = try? VNCoreMLModel(for: ModeloTesteComOutros().model) else { return }
+		let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
 			
-			self.setUpDetector()
-		}
-	}
-	
-	func checkPermission(){
-		switch AVCaptureDevice.authorizationStatus(for: .video){
-			//ja foi permitido o uso da camera antes
-		case .authorized:
-			permissionGranted = true
-			//ainda não foi permetido o uso da camera
-		case .notDetermined:
-			requestPermission()
 			
-		default:
-			permissionGranted = false
+			guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+			
+			guard let firstObservation = results.first else { return }
+			
+//			print(firstObservation.identifier, firstObservation.confidence)
+			
+			DispatchQueue.main.async {
+				self.identifierLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence * 100)"
+			}
+			
 		}
+		
+		try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
 	}
-	
-	func requestPermission(){
-		sessionQueue.suspend()
-		AVCaptureDevice.requestAccess(for: .video, completionHandler: {[unowned self] granted in
-			self.permissionGranted = granted
-			self.sessionQueue.resume()
-		})
-	}
-	
-	func setupCaptureSession() {
-		// Camera input
-		guard let videoDevice = AVCaptureDevice.default(.builtInDualWideCamera,for: .video, position: .back) else { return }
-		guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
-		
-		guard captureSession.canAddInput(videoDeviceInput) else { return }
-		captureSession.addInput(videoDeviceInput)
-		
-		// Preview layer
-		screenRect = UIScreen.main.bounds
-		
-		previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-		previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
-		previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill // Fill screen
-		previewLayer.connection?.videoOrientation = .portrait
-		
-		//aqui nós setamos a classe viewControler como o buffer delegate e conectamos o output com o nosso capture session
-		videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sampleBufferQueue"))
-		captureSession.addOutput(videoOutput)
-		
-		videoOutput.connection(with: .video)?.videoOrientation = .portrait
-		
-		// Updates to UI must be on main queue
-		DispatchQueue.main.async { [weak self] in
-			self!.view.layer.addSublayer(self!.previewLayer)
-		}
-	}
+
 }
 
-struct CameraViewControlerRepresentable : UIViewControllerRepresentable{
-	func makeUIViewController(context: Context) -> some UIViewController {
-		return CameraViewControler()
-	}
-	func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+
+struct CameraViewControlerRepresentable : UIViewControllerRepresentable {
+	func makeUIViewController(context: Context) -> UIViewController {
+		return ViewController()
+		}
+
+		func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+		}
 }
